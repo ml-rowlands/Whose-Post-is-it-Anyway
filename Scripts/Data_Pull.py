afc8b969d2ea14af7f37d88ba8fe60e26b202ed3
ml-rowlands <126630561+ml-rowlands@users.noreply.github.com>
2023-08-22 14:07:32 -0600

    Testing some updates for security

--- Scripts/Data_Pull.py
+++ Scripts/Data_Pull.py
@@ -2,10 +2,11 @@
 # Pulling data from Strava API for NLP project
 # Works in tandem with NLP.ipynb 
 
-
-#Import everything I will need for this 
+ 
 import requests
 import urllib3
+import os
+from dotenv import load_dotenv
 import numpy as np 
 import pandas as pd
 import seaborn as sns
@@ -16,13 +17,21 @@ from scipy.optimize import curve_fit
 import scipy.stats as stats
 
 
-
 #Disable warnings
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+#Get env variables
+#client_id = str(os.getenv('STRAVA_CLIENT_ID'))
+##client_secret = str(os.getenv('STRAVA_SECRET_CLIENT'))
+#michael_tok = str(os.getenv('MICHAEL_TOK'))
+#erika_tok = str(os.getenv('ERIKA_TOK'))
 
-refresh_tokens = {'Michael' : '321e0aa1e8e17d37b2867e4752aa43766474cd3a', 
-                  'Erika' :   '59df18fe42368e80e13b9686284333be69e872a2'
+print(erika_tok)
+
+
+
+refresh_tokens = {'Michael' : michael_tok, 
+                  'Erika' :  erika_tok
                     }
 
 
@@ -33,30 +42,36 @@ activites_url = "https://www.strava.com/api/v3/athlete/activities"
 datasets = {}
 
 for person, r_token in refresh_tokens.items():
+    # Initialize the person's dataset
+    datasets[person] = []
+    
+    for page in range(2):
+        # Things we need for the API to give us the data
+        payload = {
+            'client_id': "YOUR_CLIENT_ID",
+            'client_secret': 'YOUR_CLIENT_SECRET',
+            'refresh_token': r_token,
+            'grant_type': "refresh_token",
+            'f': 'json'
+        }
+
+        print("Requesting Token...\n")
+        res = requests.post(auth_url, data=payload)
+        access_token = res.json()['access_token']
+        print("Access Token = {}\n".format(access_token))
+
+        header = {'Authorization': 'Bearer ' + access_token}
+        param = {'per_page': 200, 'page': page}
+
+        data_page = pd.json_normalize(requests.get(activites_url, headers=header, params=param).json())
+        data_page['Athlete'] = person
+        datasets[person].append(data_page)
+
+# Concatenate the datasets
+df = pd.concat([pd.concat(person_data, ignore_index=True) for person_data in datasets.values()], ignore_index=True)
   
-#Things we need for the API to give us the data
-  payload = {
-      'client_id': "75238", 
-      'client_secret': '0ad2c3b5d522ef8f4dbbb89d4759efabea4f7ff6',
-      'refresh_token': r_token,
-
-      'grant_type': "refresh_token",
-      'f': 'json'
-  }
-
-  print("Requesting Token...\n")
-  res = requests.post(auth_url, data=payload, verify=False)
-  access_token = res.json()['access_token']
-  print("Access Token = {}\n".format(access_token))
-
-  header = {'Authorization': 'Bearer ' + access_token}
-  param = {'per_page': 200, 'page': 1}
-
-  datasets[person] = pd.json_normalize(requests.get(activites_url, headers=header, params=param).json())
-  datasets[person]['Athlete'] = person
-  
-  
-datasets
+#datasets
 
 
-df = pd.concat(datasets.values(), ignore_index=True)
+#df = pd.concat(datasets.values(), ignore_index=True)
+
