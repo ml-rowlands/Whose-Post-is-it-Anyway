9fdcf059f71e34d526fba3c22afb1cc0b2157f18
ml-rowlands <126630561+ml-rowlands@users.noreply.github.com>
2023-09-13 10:22:11 -0600

    Converted ipynb to py and added streamlit

---
+++ Production/Strava_Post_Author_Predictor.py
@@ -0,0 +1,96 @@
+
+# Import necessary modules
+import streamlit as st
+from joblib import load
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Load the trained model
+model = load('best_model.joblib')
+
+# App title
+st.title('Strava Post Author Predictor')
+
+# Collect user input
+## Textual features (text input)
+name = st.text_input('Title:', 'Morning Run')
+
+## Categorical features (dropdown)
+sport_type = st.selectbox('Sport Type:', ['Run', 'TrailRun','Ride', 'NordicSki', 'BackcountrySki'])
+private = st.selectbox('Privacy:', ['Private', 'Public'])
+
+
+## Numerical features (sliders)
+average_heartrate = st.slider('Average Heartrate(bpm):', min_value=100, max_value=200)
+distance = st.slider('Distance (km):', min_value=0, max_value=50)
+elapsed_time = st.slider('Workout Time (s):', min_value=0, max_value=100000)
+total_elevation_gain = st.slider('Vert (m):', min_value=0, max_value=10000)
+average_speed = st.slider('Average Speed (km/hr):', min_value=0, max_value=25)
+max_speed = st.slider('Max Speed (km/hr):', min_value=0, max_value=35)
+elev_high = st.slider('High Point Elevation (m):', min_value=0, max_value=5000)
+kudos_count = st.slider('Kudos Count:', min_value=0, max_value=100)
+athlete_count = st.slider("Total Group Size:", min_value=0, max_value=10)
+
+
+
+# Prediction button
+if st.button('Predict Author'):
+    # Initialize progress bar
+    progress_bar = st.progress(0)
+    
+    # Preprocess and bundle the input data (this will depend on your specific preprocessing steps)
+    user_data_dict = {
+        'average_heartrate': [average_heartrate],
+        'distance': [distance],
+        'name': [name],
+        'kudos_count': [kudos_count],
+        'sport_type': [sport_type],
+        'private': [private],
+        'elapsed_time': [elapsed_time],
+        'total_elevation_gain': [total_elevation_gain],
+        'average_speed': [average_speed],
+        'max_speed': [max_speed],
+        'elev_high': [elev_high],
+        'athlete_count': [athlete_count]
+    }
+    
+    user_data = pd.DataFrame(user_data_dict)
+    
+
+    # Update progress bar
+    progress_bar.progress(50)
+    
+    # Make the prediction
+    pred = model.predict_proba(user_data)
+
+    # Update progress bar
+    progress_bar.progress(100)
+    
+    # Display the prediction probabilities as a bar chart
+    st.subheader('Prediction Probabilities')
+    
+    # Extract class labels and probabilities
+    class_labels = model.classes_
+    probabilities = pred[0]
+    
+    
+    # Create a more professional bar chart
+    fig, ax = plt.subplots(figsize=(12, 8))
+    bars = ax.barh(class_labels, probabilities, color='dodgerblue')
+
+    # Add data labels
+    for bar in bars:
+        width = bar.get_width()
+        label_x_pos = width
+        ax.text(label_x_pos, bar.get_y() + bar.get_height()/2, f'{width:.2f}', va='center')
+
+    # Add grid lines and style the plot
+    ax.grid(True, linestyle='--', linewidth=0.5, color='gray')
+    ax.set_xlabel('Probability', fontsize=14)
+    ax.set_ylabel('Athlete', fontsize=14)
+    ax.set_title('Predicted Probabilities for Each Athlete', fontsize=16)
+
+    st.pyplot(fig)
+    
+
---
+++ Production/sp_model_selection.py
@@ -0,0 +1,110 @@
+import requests
+import urllib3
+import os
+import sys 
+
+import numpy as np 
+import pandas as pd
+import dfply
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Importing necessary libraries
+from sklearn.impute import SimpleImputer
+from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV, cross_val_score, train_test_split
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.compose import ColumnTransformer
+
+
+from geopy.geocoders import Nominatim, Photon
+
+sys.path.append("../")
+from Scripts.Data_Pull import *
+from Scripts.Location_Filter import *
+
+import streamlit as st
+from joblib import dump, load
+
+
+
+#Drop Bad Requests which have no name value
+df=df.dropna(axis=0, subset=['name'])
+
+#Split the data into training and testing for predictors and target
+X = df.drop('athlete', axis=1)
+y = df['athlete']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+# Step 1: Create Preprocessing Steps
+numeric_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='mean')),
+    ('scaler', StandardScaler())])
+
+text_transformer = Pipeline(steps=[
+    ('tfidf', TfidfVectorizer(max_features=500))])
+
+categorical_transformer = Pipeline(steps=[
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))])
+
+# Apply transformations to the appropriate columns
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numeric_transformer, ['distance', 'elapsed_time', 'total_elevation_gain', 'kudos_count', 'average_heartrate', 'elev_high']),
+        ('text', text_transformer, 'name'),
+        ('cat', categorical_transformer, ['sport_type', 'private'])])
+
+# Step 2: Create Pipelines
+
+pipelines= [Pipeline([('preprocess', preprocessor),
+                    ('classifier', RandomForestClassifier())]), 
+            Pipeline([('preprocess', preprocessor),
+                    ('classifier', GradientBoostingClassifier())]),
+            Pipeline([('preprocess', preprocessor),
+                    ('classifier', LogisticRegression())])]
+
+# Step 3: Hyperparameter Tuning
+param_grids = [{'classifier__n_estimators': [50, 100, 200]},
+               {'classifier__n_estimators': [50, 100, 200]},
+               {'classifier__C': [0.1, 1, 10]}]
+
+
+def train_and_select_model(X, y):
+    best_score = 0.0
+    best_pipeline = None
+    
+    for i, pipeline in enumerate(pipelines):
+        param_grid = param_grids[i]
+        grid_search = GridSearchCV(pipeline, param_grid, cv=5, scoring='accuracy')
+        grid_search.fit(X, y)
+        
+        if grid_search.best_score_ > best_score:
+            best_score = grid_search.best_score_
+            best_pipeline = grid_search.best_estimator_
+    
+    return best_pipeline
+
+
+best_pipeline = train_and_select_model(X_train, y_train)
+
+# Evaluate the model on the test set
+y_pred = best_pipeline.predict(X_test)
+test_accuracy = accuracy_score(y_test, y_pred)
+
+# Decision point
+if test_accuracy > 0.75:
+    # Refit model to the entire dataset
+    best_pipeline.fit(X,y)
+    # Save the refitted model
+    dump(best_pipeline, 'best_model.joblib')
+else:
+    # Take other actions, like revisiting feature engineering, etc.
+    print("The new model is not performing at an accuracy of 75{%} or greater, so the model was not refit!")
+
+
+
